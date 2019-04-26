a3a2d4cf5e9a73e3ef92ccccafa2b6f0a9ff5c54
Csontho Marcell <csonthomarci@gmail.com>
2019-04-26 14:50:16 +0200

    Initial commit

---
+++ People Counter/People Counter/form.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace People_Counter
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.helyi_le = new System.Windows.Forms.Label();
+            this.helyi_fel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.kulfoldi_fel = new System.Windows.Forms.Label();
+            this.kulfoldi_le = new System.Windows.Forms.Label();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Helyi:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(264, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Külföldi:";
+            // 
+            // helyi_le
+            // 
+            this.helyi_le.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.helyi_le.AutoSize = true;
+            this.helyi_le.Location = new System.Drawing.Point(132, 131);
+            this.helyi_le.Name = "helyi_le";
+            this.helyi_le.Size = new System.Drawing.Size(13, 13);
+            this.helyi_le.TabIndex = 2;
+            this.helyi_le.Tag = "foreign_counter";
+            this.helyi_le.Text = "0";
+            // 
+            // helyi_fel
+            // 
+            this.helyi_fel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.helyi_fel.AutoSize = true;
+            this.helyi_fel.Location = new System.Drawing.Point(132, 51);
+            this.helyi_fel.Name = "helyi_fel";
+            this.helyi_fel.Size = new System.Drawing.Size(13, 13);
+            this.helyi_fel.TabIndex = 3;
+            this.helyi_fel.Tag = "local_counter";
+            this.helyi_fel.Text = "0";
+            this.helyi_fel.Click += new System.EventHandler(this.helyi_fel_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.resetToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(353, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.toolStripMenuItem1.Text = "Settings";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fel:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Le:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.34247F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.65753F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 130F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.helyi_fel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.helyi_le, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.kulfoldi_fel, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.kulfoldi_le, 2, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.33334F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 82F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(353, 180);
+            this.tableLayoutPanel1.TabIndex = 11;
+            // 
+            // kulfoldi_fel
+            // 
+            this.kulfoldi_fel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.kulfoldi_fel.AutoSize = true;
+            this.kulfoldi_fel.Location = new System.Drawing.Point(280, 51);
+            this.kulfoldi_fel.Name = "kulfoldi_fel";
+            this.kulfoldi_fel.Size = new System.Drawing.Size(13, 13);
+            this.kulfoldi_fel.TabIndex = 7;
+            this.kulfoldi_fel.Text = "0";
+            // 
+            // kulfoldi_le
+            // 
+            this.kulfoldi_le.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.kulfoldi_le.AutoSize = true;
+            this.kulfoldi_le.Location = new System.Drawing.Point(280, 131);
+            this.kulfoldi_le.Name = "kulfoldi_le";
+            this.kulfoldi_le.Size = new System.Drawing.Size(13, 13);
+            this.kulfoldi_le.TabIndex = 8;
+            this.kulfoldi_le.Text = "0";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 200);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form";
+            this.Text = "People Counter";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label helyi_le;
+        private System.Windows.Forms.Label helyi_fel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label kulfoldi_fel;
+        private System.Windows.Forms.Label kulfoldi_le;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+    }
+}
+
---
+++ People Counter/People Counter/form.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace People_Counter
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public const string helyi_fel_char = "8";
+        public const string helyi_le_char = "5";
+        public const string kulfoldi_fel_char = "w";
+        public const string kulfoldi_le_char = "s";
+        protected int helyi_fel_c = 0;
+        protected int helyi_le_c = 0;
+        protected int kulfoldi_fel_c = 0;
+        protected int kulfoldi_le_c = 0;
+
+        public Form()
+        {
+            InitializeComponent();
+            this.KeyPress += keyPress;
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar.ToString())
+            {
+                case helyi_fel_char:
+                    helyi_fel_c++;
+                    helyi_fel.Text = helyi_fel_c.ToString();
+                    break;
+                case helyi_le_char:
+                    helyi_le_c++;
+                    helyi_le.Text = helyi_le_c.ToString();
+                    break;
+                case kulfoldi_fel_char:
+                    kulfoldi_fel_c++;
+                    kulfoldi_fel.Text = kulfoldi_fel_c.ToString();
+                    break;
+                case kulfoldi_le_char:
+                    kulfoldi_le_c++;
+                    kulfoldi_le.Text = kulfoldi_le_c.ToString();
+                    break;
+
+            }
+            Console.WriteLine("Key pressed:" + e.KeyChar);
+        }
+
+        private void helyi_fel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            helyi_fel_c = helyi_le_c = kulfoldi_fel_c = kulfoldi_le_c = 0;
+            helyi_fel.Text = helyi_fel_c.ToString();
+            helyi_le.Text = helyi_le_c.ToString();
+            kulfoldi_fel.Text = kulfoldi_fel_c.ToString();
+            kulfoldi_le.Text = kulfoldi_le_c.ToString();
+        }
+    }
+}
